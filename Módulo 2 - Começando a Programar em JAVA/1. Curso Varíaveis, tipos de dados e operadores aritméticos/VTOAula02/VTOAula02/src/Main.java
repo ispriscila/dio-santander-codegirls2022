567f0790717121bb356199307b6d9720eff12816
ispriscila <12496716+ispriscila@users.noreply.github.com>
2022-07-20 11:11:06 -0300

    classe de exemplo para aula 02

---
+++ Módulo 2 - Começando a Programar em JAVA/1. Curso Varíaveis, tipos de dados e operadores aritméticos/VTOAula02/VTOAula02/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Hello world!");
+
+        byte b1 = 10;
+        byte b2 = 25;
+
+        short s1 = 2345;
+        //short s2 = 567834;
+
+        //int i1 = -9847374575686;
+        int i2 = -685;
+
+        long l1 = 576869694394L;
+        //long l2 = 576869694394556678788999056444433566l;
+
+        //float f1 = 4.5;
+        float f2 = 4.5f;
+
+        double d1;
+        double d2;
+
+        char c1 = 'w';
+        //char c2 = 'dd';
+        //char c3; teste unicode
+
+        String str1 = "priscila";
+        //String str2 = 'elissandro';
+        String str3 = "elissandro priscila";
+
+        String str4 = "20/07/2022";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+
+    }
+
+
+
+}
