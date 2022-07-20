a653ce67d96eabf4268b0ad77de48fcc6936ec49
ispriscila <12496716+ispriscila@users.noreply.github.com>
2022-07-20 06:04:20 -0300

    criando Class Livro

--- Módulo 2 - Começando a Programar em JAVA/0. Dominando IDEs Java/PrimeiroProjetoIntelliJ/src/br/com/dio/PrimeiroPrograma.java
+++ Módulo 2 - Começando a Programar em JAVA/0. Dominando IDEs Java/PrimeiroProjetoIntelliJ/src/br/com/dio/PrimeiroPrograma.java
@@ -3,12 +3,50 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
+
     public static void main(String[] args) {
-        /*int a = 5;
-        int b = 3;
-        System.out.println("Heloo, World! " + (a + b));*/
+        //*//*int a = 5;
+        //int b = 3;
+        //System.out.println("Heloo, World! ");
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O problema dos 3 corpor", 300);
+        System.out.println(livro1);
+    }
+
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
